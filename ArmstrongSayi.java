6ede5863a0f04519b91b4d9dfcc1ee49c6d08028
elelifnur <81188546+elelifnur@users.noreply.github.com>
2023-08-25 14:47:17 +0300

    Add files via upload

---
+++ ArmstrongSayi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ArmstrongSayi {
+    public static  void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+
+        int toplam = 0;
+        int tempSayi = sayi;
+
+        while (tempSayi > 0) {
+            int basamak = tempSayi % 10;
+            toplam += basamak;
+            tempSayi /= 10;
+            }
+        System.out.println("Sayının basamaklarının toplamı: " + toplam);
+        }
+    }
+
+
